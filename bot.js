e1469562c346161692a00aca7adb568a97182018
Noah Halstead <nhalstead@users.noreply.github.com>
2017-10-02 23:13:32 -0400

    Added Some more Commands
    Added some more Commands and some other Options for a Basic Bot.

--- bot.js
+++ bot.js
@@ -13,22 +13,46 @@ client.on('ready', () => {
   console.log(`Logged in as ${client.user.tag}!`);
   console.log("Add me to your Server! - https://discordapp.com/oauth2/authorize?&client_id="+config.appId+"&scope=bot&permissions=0");
   console.log("\n---- Bot: Online!\n ");
+  client.user.setGame('Offline!')
 });
 
 
 // Event Listeners
 client.on('message', inMsg => {
   if (inMsg.content.toLowerCase() === 'ping') {
-    //inMsg.reply('Pong!');
-	e(inMsg, "PONG!");
+    e(inMsg, "PONG!");
   }
   
-	
+  if (inMsg.content.toLowerCase() === 'online?') {
+    e(inMsg, "Yup, I have been on for " + m2ms(client.uptime));
+  }
+  
+  // Other Stuff
+  if (inMsg.content.toLowerCase() === 'hello') {
+    e(inMsg, "Hi");
+  }
+  
+  if (inMsg.content.toLowerCase() === 'i hate you') {
+    c("Sent Message to " + inMsg.author.name);
+    s(inMsg, "Can we be Friends?");
+  }
+
+  
 });
 
 // Global Functions
-function e(c, i, o = false) {
-  if (i !== "" || o === true) { c.channel.send(i); } else { console.log("Called to respond with nothing in the Server Chat"); }
-}
+  function e(c, i, o = false) {
+    if (i !== "" || o === true) { c.channel.send(i); } else { console.log("Called to respond with nothing in the Server Chat"); }
+  }
+  function s(c, i) { if (i !== "") { c.author.send(i); } }
+  function c(m){ console.log("INFO: " + m); }
+
+  // https://stackoverflow.com/a/21294619
+  function m2ms(i) {
+    var m = Math.floor(i / 60000);
+    var s = ((i % 60000) / 1000).toFixed(0);
+    return m + ":" + (s < 10 ? '0' : '') + s;
+  }
+
 
 client.login(config.token);
