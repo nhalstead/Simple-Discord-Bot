2c58e22af80b381e2b50fc63c783dc29f6ea497f
Noah Halstead <nhalstead00@gmail.com>
2019-07-20 18:58:14 -0400

    Update index.js
    Added Support for ENV params for configuring the bot's token and appId

--- src/config/index.js
+++ src/config/index.js
@@ -3,16 +3,16 @@ const _ = require('lodash');
 module.exports = _.extend(
 	{
 		prefix: "sdb",
-		debug: false,
+		debug: process.env.DEBUG_LOG || false,
 		pmmessage: false,
 		webAdmin: {
-			enabled: false,
+			enabled: process.env.WEBADMIN || false,
 			apiKeys: [],
 			port: 8898
 		},
 		discord: {
-			appId: "",
-			token: ""
+			appId: process.env.DISCORD_APPID || "",
+			token: process.env.DISCORD_TOKEN || ""
 		}
 	},
 	(!process.env.NODE_ENV) ? require('./default') : require('./' + process.env.NODE_ENV),
